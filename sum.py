5c0d79d497be2b57ca024ad1fc784b9c4e7eedce
kartikdaksh690 <kartikdaksh267@gmil.com>
2026-08-10 18:16:55 +0530

    change a line in sum.py

--- sum.py
+++ sum.py
@@ -1,5 +1,8 @@
 a=5
 b=8
 c= a + b
-print("The sum of", a, "and", b, "is", c)
-print("created a new file sum.py to check the functionality of git commit and git push commands")
+print("The sum of", a, "and", b, "is", c)  
+
+
+
+'''created a new file sum.py to check the functionality of git commit and git push commands'''
