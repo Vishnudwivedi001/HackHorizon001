3fc19fbe0c96176e7066b30d2a5b917230c313ca
kartikdaksh690 <kartikdaksh267@gmil.com>
2026-08-10 18:14:39 +0530

    some change in redme and created a new file named as sum.ps

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Team work
+Hello ,I am kartik
+I am just testing that i can change something blah blah . :>
---
+++ sum.py
@@ -0,0 +1,5 @@
+a=5
+b=8
+c= a + b
+print("The sum of", a, "and", b, "is", c)
+print("created a new file sum.py to check the functionality of git commit and git push commands")
